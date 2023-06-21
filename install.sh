ea59150c069c5061f1a109955033c2d9d58448af
avizcaino <adrian.vizcaino@gtd.eu>
2023-06-21 18:59:53 +0000

    Edited install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ else
 fi
 
 echo "Installing RT into /usr/local/bin"
+sudo rm /usr/local/bin/rt 2> /dev/null
 sudo mkdir -p /usr/local/src/rt && sudo cp -v ./src/* /usr/local/src/rt/
 sudo cp -v rt /usr/bin/
 sudo chown root:root /usr/bin/rt
 sudo chown -R root:root /usr/local/src/rt
-sudo chmod 555 /usr/local/bin/rt
+sudo chmod 555 /usr/bin/rt
