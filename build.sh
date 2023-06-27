8d349d55abf132f09e7a004f65660543ce00011b
avizcaino <adrian.vizcaino@gtd.eu>
2023-06-27 10:24:59 +0000

    changed build number to date of build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #/bin/sh 
 
-buildnum=`git log --pretty=format:'%h' -n 1`
-echo "num = \""$buildnum"\"" > ./src/buildnum.py
-echo "Build number generated: "$buildnum
+date=$(date '+%d_%m_%Y-%H:%M:%S')
+#buildnum=`git log --pretty=format:'%h' -n 1`
+echo "num = \""$date"\"" > ./src/buildnum.py
+echo "Build number generated: "$date
