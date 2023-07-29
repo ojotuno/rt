308b3cb81ca7a1f806df343dca7168681e67c1d2
avizcaino <adrian.vizcaino@gtd.eu>
2023-07-29 11:52:30 +0000

    rt execution script is now located in src folder

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ fi
 
 echo "Installing RT into /usr/local/bin"
 sudo rm /usr/local/bin/rt 2> /dev/null
-sudo mkdir -p /usr/local/src/rt && sudo cp -v ./src/* /usr/local/src/rt/
-sudo cp -v rt /usr/bin/
+sudo mkdir -p /usr/local/src/rt && sudo cp -v ./src/* /usr/local/src/rt
+# move rt execution script into /usr/bin
+sudo mv -v /usr/local/src/rt/rt /usr/bin/
 sudo chown root:root /usr/bin/rt
 sudo chown -R root:root /usr/local/src/rt
 sudo chmod 555 /usr/bin/rt
