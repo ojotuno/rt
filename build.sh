13e68561b387177b010c20e7acbba66d060ee953
ojotuno <adrianico65@hotmail.com>
2024-03-27 10:45:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/sh 
 
-date=$(date '+%d_%m_%Y%H:%M:%S')
+date=$(date '+%d%m%Y-%H%M%S')
 #buildnum=`git log --pretty=format:'%h' -n 1`
 echo "num = \""$date"\"" > ./src/buildnum.py
 echo "Build number generated: "$date
